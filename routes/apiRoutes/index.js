0cf5d7442b71877e037636f285c9f1022a57214b
scottrohrig <scott.rohrig@gmail.com>
2022-01-22 12:58:29 -0800

    mvp

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,42 @@
+const path = require( 'path' );
+const router = require( 'express' ).Router();
+const { v4: uuid } = require( 'uuid' );
+const { createNewNote, validateNote, deleteById } = require( '../../lib/notes' );
+const { notes } = require( path.join( __dirname, '..', '..', 'db', 'db.json' ) );
+
+// console.log( path.join( __dirname, '..', '..', 'db', 'db.json' ) )
+
+// get notes
+router.get( '/notes', ( req, res ) => {
+    res.json( notes );
+} );
+
+// post notes
+router.post( '/notes', ( req, res ) => {
+    // provide unique id
+    req.body.id = uuid();
+    // console.log( 'body', req.body );
+
+    // validate req
+    if ( !validateNote( req.body ) ) {
+        res.status( 400 ).send( 'The note is not properly formatted.' )
+    } else {
+        // create new note
+        const note = createNewNote( req.body, notes );
+        // and respond
+        res.json( note );
+    }
+} );
+
+// delete note route
+router.delete( '/notes/:id', ( req, res ) => {
+    // validate id
+    console.log( 'deleting note by id', req.params.id )
+
+    // filter notes by id
+    deleteById( req.params.id, notes )
+    res.json( { message: 'deleted' } );
+} );
+
+// modularize
+module.exports = router;
---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,22 @@
+// html route handling
+const router = require( 'express' ).Router();
+const path = require( 'path' );
+const { runInNewContext } = require( 'vm' );
+const public = path.join( __dirname, '../', '../', 'public' );
+
+// root
+router.get( '/', ( req, res ) => {
+    res.sendFile( path.join( public, 'index.js' ) );
+} );
+
+// notes
+router.get( '/notes', ( req, res ) => {
+    res.sendFile( path.join( public, 'notes.html' ) );
+} );
+
+//  default
+router.get( '/', ( req, res ) => {
+    res.sendFile( path.join( public, 'index.js' ) );
+} );
+
+module.exports = router;
